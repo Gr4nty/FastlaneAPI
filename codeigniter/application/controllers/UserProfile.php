6627e8b50d3a0e8278f929e8cd7099551d8f9b63
Gergo Illes <grantys@gmail.com>
2014-06-24 19:32:37 +0200

    Codeigniter REST setup

---
+++ codeigniter/application/controllers/UserProfile.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+require APPPATH . '/libraries/REST_Controller.php';
+
+class UserProfile extends REST_Controller {
+
+    
+}
+
---
+++ codeigniter/application/models/UserProfile_model.php
@@ -0,0 +1,6 @@
+<?php
+
+class UserProfile_model extends CI_Model {
+
+}
+
